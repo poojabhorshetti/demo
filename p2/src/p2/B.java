1966ba7a88deaa1e553f57a336dfa652603e2998
demo <bhorshettipooja@gmail.com>
2024-11-19 16:10:52 +0530

    Update B.java

--- p2/src/p2/B.java
+++ p2/src/p2/B.java
@@ -4,6 +4,8 @@ public class B {
 	public static void main(String args[])
 	{
 		System.out.println("hi pooja here...m learning java ....now m studying on github process");
+		System.out.println("i made some changes here....!!!!");
+		System.out.println("now my aim is to enter in IT Field");
 	}
 
 }
